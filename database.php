0643c5c5b38ea3f501e88c224f02925c1db96eea
Istabraq <angel.ofmoons@gmail.com>
2017-06-29 10:43:03 -0400

    Fix db settings and make the project work

--- database.php
+++ database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=my_guitar_shop1';
-    $username = 'mgs_user';
-    $password = 'pa55word';
+    $dsn = 'mysql:host=sql2.njit.edu;dbname=ia238';
+    $username = 'ia238';
+    $password = 'qMSAi5hus';
 
     try {
         $db = new PDO($dsn, $username, $password);
