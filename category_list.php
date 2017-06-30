f168819609edde2e785f3b6c9968c269d0269220
Istabraq <angel.ofmoons@gmail.com>
2017-06-30 02:54:59 -0400

    Provide a category creation form in the category list page and style it to show as intended.

--- category_list.php
+++ category_list.php
@@ -41,9 +41,15 @@ $statement->closeCursor();
     </table>
 
     <h2>Add Category</h2>
-    
-    <!-- add code for the form here -->
-    
+        <form action="add_category.php" method="post"
+              id="add_category_form">
+
+            <label>Name:</label>
+            <input type="text" name="name"><br>
+
+            <label>&nbsp;</label>
+            <input type="submit" value="Add"><br>
+        </form>
     <br>
     <p><a href="index.php">List Products</a></p>
 
